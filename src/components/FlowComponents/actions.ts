cb1c691d3bc9a84f37cae6ee5efeb027f83d64e0
Samrath Chauhan <samrathchauhan49@gmail.com>
2024-05-18 11:25:31 +0530

    feat: Update the flow control and required features.

---
+++ src/components/FlowComponents/actions.ts
@@ -0,0 +1,91 @@
+import { addEdge, MarkerType, Node } from 'reactflow'
+import { Dispatch } from 'redux'
+import { loadNodes, addNode, nodesDeleted } from '@/slices/nodeSlice'
+
+// Function to handle new edge connections
+export const handleConnect = (params, setEdges) => {
+  setEdges(eds => {
+    console.log('@edge', eds)
+    return addEdge(
+      {
+        ...params,
+        markerEnd: { type: MarkerType.Arrow },
+      },
+      eds
+    )
+  })
+}
+
+// Function to generate unique ID for new nodes
+export const getId = (storeNodes, nodes) => {
+  if (storeNodes?.length > nodes?.length) {
+    return storeNodes.length.toString()
+  }
+  return nodes?.length.toString()
+}
+
+// Function to handle drag over event
+export const handleDragOver = event => {
+  event.preventDefault()
+  event.dataTransfer.dropEffect = 'move'
+}
+
+// Function to handle drop event
+export const handleDrop = (
+  event,
+  flowRef,
+  reactFlowInstance,
+  dispatch: Dispatch,
+  getId: () => string,
+  setSelectedNode: (id: string) => void
+) => {
+  event.preventDefault()
+  const reactFlowBounds = flowRef.current.getBoundingClientRect()
+  const type =
+    'messageNode' || event.dataTransfer.getData('application/reactflow')
+  const label = event.dataTransfer.getData('content')
+  const position = reactFlowInstance.project({
+    x: event.clientX - reactFlowBounds.left,
+    y: event.clientY - reactFlowBounds.top,
+  })
+  const newNode: Node = {
+    id: getId(),
+    type,
+    position,
+    data: { heading: 'Send Message', content: label },
+  }
+  dispatch(addNode(newNode))
+  setSelectedNode(newNode.id)
+}
+
+// Function to handle node deletion
+export const handleNodesDelete = (
+  deletedNodes,
+  nodes,
+  edges,
+  dispatch: Dispatch,
+  setNodes,
+  setEdges
+) => {
+  const remainingNodes = nodes.filter(node => !deletedNodes.includes(node))
+  const remainingEdges = edges.filter(
+    edge =>
+      !deletedNodes.some(
+        node => edge.source === node.id || edge.target === node.id
+      )
+  )
+  console.log('@remaining', remainingNodes)
+  dispatch(loadNodes(remainingNodes))
+  dispatch(nodesDeleted(true))
+  setEdges(remainingEdges)
+}
+
+// Function to handle key down event (for deleting nodes)
+export const handleKeyDown = (event, nodes, handleNodesDelete) => {
+  if (event.key === 'Delete') {
+    const selectedNodes = nodes.filter(node => node.selected)
+    if (selectedNodes.length > 0) {
+      handleNodesDelete(selectedNodes)
+    }
+  }
+}
--- src/components/FlowComponents/index.tsx
+++ src/components/FlowComponents/index.tsx
@@ -1,45 +1,150 @@
 'use client'
-import type { OnConnect } from 'reactflow'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import {
   Background,
-  Controls,
   ReactFlow,
-  addEdge,
   useNodesState,
   useEdgesState,
+  ReactFlowProvider,
 } from 'reactflow'
+import { useDispatch, useSelector } from 'react-redux'
 import { initialNodes, nodeTypes } from './Nodes'
 import { initialEdges, edgeTypes } from './Edges'
+import { RootState } from '@/store'
+import {
+  handleConnect,
+  getId,
+  handleDragOver,
+  handleDrop,
+  handleNodesDelete,
+  handleKeyDown,
+} from './actions'
+import { loadNodes } from '@/slices/nodeSlice'
 
-/**
- * React Flow component
- *
- * This is the main component of the app. It uses the React Flow library
- * to render the nodes and edges. It also has controls for zooming and panning.
- *
- */
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes)
+  const dispatch = useDispatch()
+  const flowRef = useRef(null)
+  const textRef = useRef(null)
+
+  // State from Redux store
+  const storeNodes = useSelector((state: RootState) => state.nodes.nodes)
+
+  // Local state hooks
+  const [nodeText, setNodeText] = useState('')
+  const [nodes, setNodes, onNodesChange] = useNodesState(storeNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
-  const onConnect: OnConnect = useCallback(
-    connection => setEdges(edges => addEdge(connection, edges)),
+  const [selectedNode, setSelectedNode] = useState(null)
+  const [isSelected, setIsSelected] = useState(false)
+  const [reactFlowInstance, setReactFlowInstance] = useState(null)
+  const [nodeName, setNodeName] = useState('Node 1')
+
+  console.log('@state', storeNodes, edges)
+
+  const onConnect = useCallback(
+    params => handleConnect(params, setEdges),
     [setEdges]
   )
 
+  const onInit = reactFlowInstance => setReactFlowInstance(reactFlowInstance)
+
+  const onDragOver = handleDragOver
+
+  const onDrop = event =>
+    handleDrop(
+      event,
+      flowRef,
+      reactFlowInstance,
+      dispatch,
+      () => getId(storeNodes, nodes),
+      setSelectedNode
+    )
+
+  const onNodesDelete = useCallback(
+    deletedNodes =>
+      handleNodesDelete(
+        deletedNodes,
+        nodes,
+        edges,
+        dispatch,
+        setNodes,
+        setEdges
+      ),
+    [nodes, edges, dispatch, setNodes, setEdges]
+  )
+
+  const handleKeyDownCallback = useCallback(
+    event => handleKeyDown(event, nodes, onNodesDelete),
+    [nodes, onNodesDelete]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDownCallback)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDownCallback)
+    }
+  }, [handleKeyDownCallback])
+
+  useEffect(() => {
+    if (storeNodes.length === 0 && initialNodes.length > 0) {
+      dispatch(loadNodes(initialNodes))
+    } else {
+      setNodes(storeNodes)
+    }
+  }, [dispatch, storeNodes, initialNodes, setNodes])
+
+  useEffect(() => {
+    const node = nodes.find(node => node.selected)
+    if (node) {
+      setSelectedNode(node)
+      setIsSelected(true)
+    } else {
+      setSelectedNode(null)
+      setIsSelected(false)
+    }
+  }, [nodes])
+
+  useEffect(() => {
+    setNodeName(selectedNode?.data?.content || '')
+  }, [selectedNode])
+
+  useEffect(() => {
+    textRef?.current?.focus()
+  }, [selectedNode])
+
+  useEffect(() => {
+    setNodes(nds =>
+      nds.map(node => {
+        if (node.id === selectedNode?.id) {
+          return {
+            ...node,
+            data: { ...node.data, content: nodeName || ' ' },
+          }
+        }
+        return node
+      })
+    )
+  }, [nodeName, setNodes])
+
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <Controls />
-    </ReactFlow>
+    <ReactFlowProvider>
+      <div className='h-full flex-grow' ref={flowRef}>
+        <ReactFlow
+          nodes={nodes}
+          nodeTypes={nodeTypes}
+          onNodesChange={onNodesChange}
+          onInit={onInit}
+          edges={edges}
+          onNodesDelete={onNodesDelete}
+          edgeTypes={edgeTypes}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          attributionPosition='top-right'
+        >
+          <Background color='#fff' />
+        </ReactFlow>
+      </div>
+    </ReactFlowProvider>
   )
 }
