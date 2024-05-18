6affbee679f0c28eea759c22f765f8420ae8a544
Samrath Chauhan <samrathchauhan49@gmail.com>
2024-05-18 20:56:28 +0530

    fix: MessageNode component to switch styles based on node selection.

--- src/components/FlowComponents/Nodes/MessageNode.tsx
+++ src/components/FlowComponents/Nodes/MessageNode.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { MessageIcon, WhatsappIcon } from '@/icons'
+import { RootState } from '@/store'
 import React, { memo } from 'react'
+import { useSelector } from 'react-redux'
 import { Handle, Position } from 'reactflow'
 
 const MessageNode = ({ data, selected }) => {
+  const { selectedNodeId } = useSelector((state: RootState) => state.nodes)
+
   return (
     <div
-      className={`overflow-hidden rounded-lg shadow-xl ${selected ? 'border-2 border-emerald-500 shadow-2xl' : 'border-2 border-transparent'} `}
+      className={`overflow-hidden rounded-lg shadow-xl ${selected && selectedNodeId !== null ? 'border-2 border-emerald-500 shadow-2xl' : 'border-2 border-transparent'} `}
     >
       <div className='flex flex-col'>
         <div className='flex items-center justify-between gap-20 bg-emerald-100 p-2'>
