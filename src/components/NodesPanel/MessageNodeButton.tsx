49596e548c720b4dab64bb002e243b83e9f3560e
Samrath Chauhan <samrathchauhan49@gmail.com>
2024-05-18 20:55:50 +0530

    update: Node panel to make use of updated node settings panel.

--- src/components/NodesPanel/MessageNodeButton.tsx
+++ src/components/NodesPanel/MessageNodeButton.tsx
@@ -9,13 +9,15 @@ const MessageNodeButton = () => {
     event.dataTransfer.effectAllowed = 'move'
   }
   return (
-    <div
-      draggable
-      className='flex h-fit min-w-44 cursor-pointer flex-col items-center justify-between gap-2 rounded-lg border-[1px] border-gray-300 p-3'
-      onDragStart={event => onDragStart(event, 'node', 'message')}
-    >
-      <MessageIcon className='size-8' />
-      Message
+    <div className='p-5'>
+      <div
+        draggable
+        className='flex h-fit min-w-44 cursor-pointer flex-col items-center justify-between gap-2 rounded-lg border-2 border-gray-300 p-3'
+        onDragStart={event => onDragStart(event, 'node', 'message')}
+      >
+        <MessageIcon className='size-8' />
+        Message
+      </div>
     </div>
   )
 }
--- src/components/NodesPanel/index.tsx
+++ src/components/NodesPanel/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import MessageNodeButton from './MessageNodeButton'
+import NodeSettings from './NodeSettings'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store'
+import { getSelectedNode } from '../FlowComponents/actions'
 
 const NodesPanel = () => {
+  const { storeNodes, selectedNodeId } = useSelector(
+    (state: RootState) => state.nodes
+  )
+  const [selectedNode, setSelectedNode] = useState(null)
+
+  useEffect(() => {
+    if (!!selectedNode) {
+      setSelectedNode(getSelectedNode(storeNodes, selectedNodeId))
+    }
+  }, [storeNodes, selectedNode, selectedNodeId])
+
   return (
-    <div className='flex size-full min-w-[30rem] grow flex-wrap border-[1px] border-gray-300 p-5'>
-      <MessageNodeButton />
+    <div className='flex size-full min-w-[30rem] grow flex-wrap border-2 border-gray-300'>
+      {selectedNodeId !== null && selectedNodeId !== '' ? (
+        <NodeSettings />
+      ) : (
+        <MessageNodeButton />
+      )}
     </div>
   )
 }
