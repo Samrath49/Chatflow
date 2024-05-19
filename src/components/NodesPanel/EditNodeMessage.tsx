17d93988b001a4ff63de312d01db65cb0c7eaf84
Samrath Chauhan <samrathchauhan49@gmail.com>
2024-05-19 11:23:16 +0530

    feat: EditNodeMessage for changing and saving node messages.

--- src/components/NodesPanel/EditNodeMessage.tsx
+++ src/components/NodesPanel/EditNodeMessage.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+'use client'
+import { RootState } from '@/store'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSelectedNode } from '../FlowComponents/actions'
+import { updateNodeMessage } from '@/slices/nodeSlice'
+import useDebounce from '@/services/useDebounce'
 
 const EditNodeMessage = () => {
+  const { storeNodes, selectedNodeId } = useSelector(
+    (state: RootState) => state.nodes
+  )
+  
+  const [nodeMessageText, setNodeMessageText] = useState('')
+  const dispatch = useDispatch()
+
+  const debouncedNodeMessageText = useDebounce(nodeMessageText, 2000)
+
+  useEffect(() => {
+    if (selectedNodeId) {
+      const node = getSelectedNode(storeNodes, selectedNodeId)
+      setNodeMessageText(node?.data?.content || '')
+    }
+  }, [storeNodes, selectedNodeId])
+
+  useEffect(() => {
+    if (debouncedNodeMessageText !== '') {
+      dispatch(
+        updateNodeMessage({
+          id: selectedNodeId,
+          message: debouncedNodeMessageText,
+        })
+      )
+    }
+  }, [debouncedNodeMessageText, dispatch, selectedNodeId])
+
+  const handleMessageChange = e => {
+    setNodeMessageText(e.target.value)
+  }
+
   return (
     <div className='relative'>
       <textarea
         id='id-01'
         name='id-01'
-        placeholder='Write your message'
+        placeholder='Write your message.'
         rows={3}
+        value={nodeMessageText}
+        onChange={handleMessageChange}
         className='peer relative w-full rounded border border-slate-200 px-4 py-2 text-sm text-slate-500 outline-none transition-all placeholder:text-transparent autofill:bg-white invalid:border-orange-500 invalid:text-orange-700 focus:border-emerald-500 focus:outline-none invalid:focus:border-orange-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400'
       ></textarea>
       <label
--- src/components/NodesPanel/index.tsx
+++ src/components/NodesPanel/index.tsx
@@ -1,22 +1,14 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import MessageNodeButton from './MessageNodeButton'
 import NodeSettings from './NodeSettings'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store'
-import { getSelectedNode } from '../FlowComponents/actions'
 
 const NodesPanel = () => {
-  const { storeNodes, selectedNodeId } = useSelector(
-    (state: RootState) => state.nodes
+  const selectedNodeId = useSelector(
+    (state: RootState) => state.nodes.selectedNodeId
   )
-  const [selectedNode, setSelectedNode] = useState(null)
-
-  useEffect(() => {
-    if (!!selectedNode) {
-      setSelectedNode(getSelectedNode(storeNodes, selectedNodeId))
-    }
-  }, [storeNodes, selectedNode, selectedNodeId])
 
   return (
     <div className='flex size-full min-w-[30rem] grow flex-wrap border-2 border-gray-300'>
